4d6e63a0307bf3fa139968019d767416420a5ec2
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-01-10 15:30:52 +0100

    simple asio client

--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+string get_data(tcp::socket& socket){
+    boost::asio::streambuf buf;
+    read_until(socket,buf,"\n");
+    string data = buffer_cast<const char*>(buf.data());
+    return data;
+}
+
+void send_data(tcp::socket& socket, const string& message){
+    write(socket, buffer(message + "\n"));
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    io_service service;
+    tcp::socket  client_socket(service);
+
+    client_socket.connect(tcp::endpoint(address::from_string("127.0.0.1"),9999));
+
+    cout << "Enter username: ";
+    string u_name, replay, response;
+    getline(cin, u_name);
+
+    send_data(client_socket,u_name);
+
+    while (true){
+
+        response = get_data(client_socket);
+
+        response.pop_back();
+
+        if(response == "exit"){
+            cout << "Connection terminated \n";
+            break;
+        }
+
+        cout << "Server: " << response << "\n";
+
+        cout << u_name << ": ";
+        getline(cin,replay);
+        send_data(client_socket,replay);
+
+        if(replay == "exit"){
+            break;
+        }
+    }
+
     return 0;
 }
